0ffc52e8af8d4c8fd6bb8b19b3fb80dd9152e7ef
RDOSIL\rdosil <gelo.dosil@gmail.com>
2025-03-03 16:59:52 +0100

    cambios en contactos del footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,11 +11,11 @@ const Footer = () => {
             <h3 className="text-xl font-semibold mb-4">Contacto</h3>
             <div className="flex items-center space-x-2">
               <MapPin className="w-5 h-5" />
-              <p className="text-sm">Rúa do Vilar, 54, 27004 Lugo</p>
+              <p className="text-sm">Lariño 362 Carnota, 15292, A Coruña</p>
             </div>
             <div className="flex items-center space-x-2">
               <Phone className="w-5 h-5" />
-              <p className="text-sm">982 22 33 33</p>
+              <p className="text-sm">636 48 83 37</p>
             </div>
             <div className="flex items-center space-x-2">
               <Mail className="w-5 h-5" /> 

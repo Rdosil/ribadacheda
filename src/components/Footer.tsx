9c893ba267d47c7fc7f783e17cf4e8618794b271
RDOSIL\rdosil <gelo.dosil@gmail.com>
2025-03-03 16:53:02 +0100

    cambio chorra

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,7 +18,7 @@ const Footer = () => {
               <p className="text-sm">982 22 33 33</p>
             </div>
             <div className="flex items-center space-x-2">
-              <Mail className="w-5 h-5" />
+              <Mail className="w-5 h-5" /> 
               <p className="text-sm">ribadacheda@gmail.com</p>
             </div>
           </div>
